45695b6460975a412c97893493f21cd7c163b6c5
abhishek0007 <hellocoolabhishek@gmail.com>
2016-04-22 08:26:19 +0530

    ActivityA -> to ActivityB

--- app/src/main/java/gpstracking/example/com/pomodoro/MainActivity.java
+++ app/src/main/java/gpstracking/example/com/pomodoro/MainActivity.java
@@ -1,21 +1,28 @@
 package gpstracking.example.com.pomodoro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-   Button b ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b = (Button)findViewById(R.id.button);
-       b.setOnClickListener(this);
+
         Log.d("Vivz","Oncreate Activity");
+    }
+
+
+    public void gotoNexr(View v)
+    {
+        Intent i = new Intent(this,Main2Activity.class);
+        startActivity(i);
+
     }
 
 
@@ -55,8 +62,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.d("Vivz", "OnDestroyActivity");
     }
 
-    @Override
-    public void onClick(View v) {
-        Log.d("Here i am","My Button");
-    }
+
 }
